3ab3653bdf4a98fc96e37bae1da4113b1a0d55f1
wanghongyu <why318why@163.com>
2016-12-16 12:20:41 +0800

    fix:repetition priming

--- runpy.sh
+++ runpy.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 while true; do
     NUM=`ps aux | grep local_index.py | grep -v grep |wc -l`
-    if [ "${NUM}" -lt "1" ];then
-        python -u ./local_index.py
+    if [ "${NUM}" == "0" ];then
+        nohup python -u ./local_index.py &
+        sleep 5
     fi
 done
